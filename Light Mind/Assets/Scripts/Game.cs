a173ce7349244e2d7a94e4978c239811bf7bd204
Boris <bobaseoul@hotmail.fr>
2018-02-05 21:54:13 +0100

    Add Game script to manage win conditions and track GameObjects, add new tag objective

---
+++ Light Mind/Assets/Scripts/Game.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Objects;
+
+public class Game : MonoBehaviour
+{
+	// Temp variable to avoid log spamming
+	public bool LevelCompleted;
+
+	private Objective[] _objectives;
+
+	// Should initialize or count other gameObjects to limit available gameObjects for Player
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		LevelCompleted = false;
+		getAllObjectives ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isLevelCompleted ()) {
+			if (LevelCompleted)
+				return;
+			Debug.Log ("Level Completed!");
+			LevelCompleted = true;
+		}
+	}
+
+	// Find the parent GameObject of all Quad Objects and get all objectives references
+	private void getAllObjectives ()
+	{
+		_objectives = GameObject.FindGameObjectWithTag ("objective")
+							    .GetComponentsInChildren<Objective> ();
+	}
+
+	// Checks for a win condition (all objectives completed)
+	private bool isLevelCompleted ()
+	{
+		foreach (var objective in _objectives) {
+			if (!objective.Completed) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
