a8c157109b29594ec17dabb4789b7c64ae1c6c81
Jean Treibert <jeant@ouicar.fr>
2018-02-05 13:26:31 +0100

    Bind menu buttons to actions.

--- Light Mind/Assets/MainMenu.cs
+++ Light Mind/Assets/MainMenu.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	public void PlayGame () {
+		SceneManager.LoadScene(1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void QuitGame () {
+		Application.Quit();
 	}
 }
