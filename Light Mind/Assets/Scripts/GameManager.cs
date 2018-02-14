0b3717fc8290d4719f9353e69b2bbb1b0ea1307c
Lucas Moisan <lucas.moisan@epita.fr>
2018-02-14 23:21:06 +0100

    Added the draggable attribute to the json levels.

--- Light Mind/Assets/Scripts/GameManager.cs
+++ Light Mind/Assets/Scripts/GameManager.cs
@@ -173,7 +173,7 @@ public class GameManager : MonoBehaviour
                 Vector3 pos = new Vector3(jsonEntity["X"].n, jsonEntity["Y"].n, 0);
                 objectInstance.transform.position = pos;
                 DragAndDrop dragAndDrop = objectInstance.GetComponentInChildren<DragAndDrop>();
-                dragAndDrop.IsDraggable = false;
+                dragAndDrop.IsDraggable = jsonEntity["Draggable"].b;
                 BoardManager.AddItemPosition(pos);
             }
         }
