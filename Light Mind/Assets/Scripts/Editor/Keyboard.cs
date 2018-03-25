f310137eecb0299b356ebb9bda61c011687c94ab
Jules Aubert <aubertjules@gmail.com>
2018-03-25 19:27:48 +0200

    Added a script to open keyboard to save new level.

---
+++ Light Mind/Assets/Scripts/Editor/Keyboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour
+{
+
+    TouchScreenKeyboard keyboard;
+    public Text levelName;
+
+    // Use this for initialization
+    public void OpenKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
