54febfcd865b6a2231b78606f1ca287a34022a7b
Jules Aubert <aubertjules@gmail.com>
2018-03-28 12:20:33 +0200

    Added code to keyboard

--- Light Mind/Assets/Scripts/LevelEditor/InputKeyboardEditor.cs
+++ Light Mind/Assets/Scripts/LevelEditor/InputKeyboardEditor.cs
@@ -1,24 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class KeyboardInput : MonoBehaviour
+public class InputKeyboardEditor : MonoBehaviour
 {
 
     TouchScreenKeyboard keyboard;
     public string levelName;
 
-    // Use this for initialization
     public void OpenKeyboard()
     {
         keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
 
     }
 
+    public void Save()
+    {
+        // Check that the level is doable
+        // Count the number of elements
+        // Serialize to a JSON file
+    }
+
     // Update is called once per frame
     void Update()
-    {
+    {   
         if (TouchScreenKeyboard.visible == false && keyboard != null)
         {
             if (keyboard.done)
