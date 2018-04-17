90b1ac137371cc073c83cbbce83b63741ec99a55
juan <mrpandat@hotmail.fr>
2018-04-17 15:18:57 +0200

    Lock the levels in selector when not done.

--- Light Mind/Assets/Scripts/Menu/LevelItem.cs
+++ Light Mind/Assets/Scripts/Menu/LevelItem.cs
@@ -6,12 +6,17 @@ public class LevelItem : MonoBehaviour
 {
     public string name;
     public string filename;
+    public bool isInteractable = false;
 
     public void Select()
     {
-        Debug.LogWarning(this.filename);
-  			PlayerPrefs.SetString("currentLevel", this.filename);
-  			Application.LoadLevel("Scenes/GameTD3D");
+      Debug.LogWarning("isInteractable");
+      Debug.LogWarning(isInteractable);
+      if (!isInteractable) {
+        return;
+      }
+			PlayerPrefs.SetString("currentLevel", this.filename);
+			Application.LoadLevel("Scenes/GameTD3D");
     }
 
     public void SetName(string name)
@@ -23,5 +28,12 @@ public class LevelItem : MonoBehaviour
     public void SetFilename(string filename)
     {
         this.filename = filename;
+    }
+
+    public void SetInteractable(bool interactable)
+    {
+        var _button = this.GetComponentInChildren<Button>();
+        this.isInteractable = interactable;
+        _button.interactable = this.isInteractable;
     }
 }
