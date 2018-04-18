cbe5f36429636f21d2b0bcbe6bb808ec74cb9969
TimTim12 <bureau_t@epita.fr>
2018-04-18 13:37:54 +0200

    Add sound when enemy arrive on the end point

--- Light Mind/Assets/Scripts/TD/EndPoint.cs
+++ Light Mind/Assets/Scripts/TD/EndPoint.cs
@@ -4,6 +4,8 @@ namespace TD
 {
     public class EndPoint : MonoBehaviour
     {
+        private AudioSource[] _audioSources;
+
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log(string.Format("End triggered by {0}", other.gameObject));
@@ -11,6 +13,8 @@ namespace TD
             if (!other.CompareTag("enemy")) return;
             
             Destroy(other.gameObject);
+            _audioSources = GetComponents<AudioSource>();
+            _audioSources[0].Play();
             GameManager.Instance.TdManager.DecreaseLives();
         }
     }
