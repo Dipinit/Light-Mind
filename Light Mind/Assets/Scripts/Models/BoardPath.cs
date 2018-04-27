c23a408bc1c53b4e4e8ad25ffe83fb8e21be6177
Jules Aubert <aubertjules@gmail.com>
2018-04-27 19:15:33 +0200

    Added documentation to BoardPath.cs.

--- Light Mind/Assets/Scripts/Models/BoardPath.cs
+++ Light Mind/Assets/Scripts/Models/BoardPath.cs
@@ -8,18 +8,37 @@ namespace Models
         public Vector2Int Start;
         public Vector2Int End;
 
+        /// <summary>
+        /// Compute the path from two points.
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
         public BoardPath(int x1, int y1, int x2, int y2)
         {
             Start = new Vector2Int(x1, y1);
             End = new Vector2Int(x2, y2);
         }
 
+        /// <summary>
+        /// Compute the scale of a path.
+        /// </summary>
+        /// <param name="cellSize"></param>
+        /// <param name="cellOffset"></param>
+        /// <returns></returns>
         public Vector3 GetPathScale(int cellSize, int cellOffset)
         {
             return new Vector3(Mathf.Abs(End.x - Start.x) * (cellSize + cellOffset) + cellSize, 1.0f,
                 Mathf.Abs(End.y - Start.y) * (cellSize + cellOffset) + cellSize);
         }
         
+        /// <summary>
+        /// Returns the position of a path.
+        /// </summary>
+        /// <param name="cellSize"></param>
+        /// <param name="cellOffset"></param>
+        /// <returns></returns>
         public Vector3 GetPathPosition(int cellSize, int cellOffset)
         {
             var pathPosition = GetPathScale(cellSize, cellOffset);
